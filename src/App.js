ba4701af3de53d5de95ab6f371daa2a86fc34e90
ppreedam <sk6201184579@gmail.com>
2023-02-09 12:27:35 +0530

    add topbar

--- src/App.js
+++ src/App.js
@@ -5,21 +5,7 @@ import Home from './Pages/Home/Home';
 function App() {
   return (
     <div className="App">
-      {/* <h1>Hello chat</h1>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      
       <Home/>
     </div>
   );
